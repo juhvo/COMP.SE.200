d73400152791c92fded61ce0bb8bc31ab6a58831
Juho Hautaniemi <juho.hautaniemi@tuni.fi>
2021-11-28 15:15:25 +0200

    Test add with one undefined parameter

--- test/add.test.js
+++ test/add.test.js
@@ -5,4 +5,12 @@ describe("add", () => {
         const result = add(undefined, undefined)
         expect(result).toEqual(0)
     })
+    it("should handle on defined and other undefined input", () => {
+        const result = add(2, undefined)
+        expect(result).toEqual(2)
+    })
+    it("should handle on undefined and other defined input", () => {
+        const result = add(undefined, 2)
+        expect(result).toEqual(2)
+    })
 })
